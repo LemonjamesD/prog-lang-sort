cf4e208b99ee36a43e1ea172e370b16d39853e19
LemonjamesD <lemon@lemonjamesd.com>
2023-09-26 21:45:42 -0400

    More initial stuff

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,22 +1,11 @@
 use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
 use actix_files::Files;
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(echo) // From getting started
-            .route("/hey", web::get().to(manual_hello)) // From getting started
-            .service(Files::new("/", "www/dist") // File server needed for Svelte GUI, has to be created as last
+            .service(Files::new("/", "client/dist") // File server needed for Svelte GUI, has to be created as last
                 .prefer_utf8(true)
                 .index_file("index.html")
             )
