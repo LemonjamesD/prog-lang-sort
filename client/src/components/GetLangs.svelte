c96c7e81197dc169f9a1add50126c184bc3c0843
LemonjamesD <lemon@lemonjamesd.com>
2023-09-26 23:11:01 -0400

    Added posting to echo

---
+++ client/src/components/GetLangs.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+function send_get() {
+    const url: String = "http://localhost:8080/echo";
+    const data: JSON = {
+      "data": "Hello, World!"
+    };
+
+    var http_req: XMLHttpRequest = new XMLHttpRequest();
+    http_req.onreadystatechange = () => { 
+        if (http_req.readyState == 4 && http_req.status == 200)
+            console.log(http_req.response);
+        else if (http_req.readState == 4)
+            console.log("Could not fetch the data");
+    }
+    http_req.open("POST", url);
+    http_req.send(JSON.stringify(data));
+    console.log(http_req);
+}
+
+</script>
+
+<input on:click={send_get} type="button" value="Button"/>
